7b14750094807a2a4fa828d15a8443a8558284cd
jiagenli <jiagenli@foxmail.com>
2022-09-19 22:30:38 +0800

    LC42

---
+++ src/main/com/ljg/leetcode/LC42.java
@@ -0,0 +1,54 @@
+package main.com.ljg.leetcode;
+
+public class LC42 {
+    /**
+     * 从左扫描一遍，从右扫描一遍，再计算左右差值
+     * @param height
+     * @return
+     */
+    public int trap(int[] height) {
+        int len = height.length;
+        int[] leftMax = new int[len];
+        int[] rightMax = new int[len];
+        leftMax[0] = height[0];
+        for (int i = 1; i < len; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        rightMax[len - 1] = height[len - 1];
+        for (int j = len - 2; j >= 0; j--) {
+            rightMax[j] = Math.max(rightMax[j + 1], height[j]);
+        }
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+    /**
+     * 这个版本取消了左边最高点的遍历，但是提交结果来看和上一个方法没有区别。
+     * @param height
+     * @return
+     */
+    public int trap2(int[] height) {
+        int len = height.length;
+        int maxLeft = 0;
+        int[] maxRight = new int[len];
+
+        maxRight[len - 1] = height[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i + 1], height[i]);
+        }
+        int sum = 0;
+        for (int i = 1; i < len; i++) {
+            maxLeft = Math.max(height[i - 1], maxLeft);
+            int minus = Math.min(maxLeft, maxRight[i]);
+            if (minus > height[i]) {
+                sum += minus - height[i];
+            }
+        }
+        return sum;
+    }
+
+
+}
