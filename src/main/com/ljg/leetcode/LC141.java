40807986a6aa3e945426a84773d36a0da81b3390
jiagenli <jiagenli@foxmail.com>
2022-10-30 22:24:02 +0800

    LC141

---
+++ src/main/com/ljg/leetcode/LC141.java
@@ -0,0 +1,23 @@
+package main.com.ljg.leetcode;
+
+public class LC141 {
+    /**
+     * 快慢指针
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head.next, fast = head.next.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
