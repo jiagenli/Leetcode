4ee4add32ce917ebb16f4f99e774987a70edf4d6
jiagenli <jiagenli@foxmail.com>
2022-11-07 15:06:55 +0800

    LC198-2

--- src/main/com/ljg/leetcode/LC198.java
+++ src/main/com/ljg/leetcode/LC198.java
@@ -22,4 +22,23 @@ public class LC198 {
         }
         return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
     }
+
+    /**
+     * 官方题解的做法，我还是想复杂了
+     * @param nums
+     * @return
+     */
+    public int rob2(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        // 这里是一个注意点
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
+    }
 }
