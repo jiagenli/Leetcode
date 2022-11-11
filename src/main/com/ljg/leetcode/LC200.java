50d11acf553ad9790379f524384ca2a2cf9a43e8
jiagenli <jiagenli@foxmail.com>
2022-11-11 13:16:29 +0800

    LC200

---
+++ src/main/com/ljg/leetcode/LC200.java
@@ -0,0 +1,32 @@
+package main.com.ljg.leetcode;
+
+public class LC200 {
+    /**
+     * 思路是讲所有与第一个'1'相连的'1'都变为0，然后统计数组中'1'的个数
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(i, j, grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int row, int col, char[][] grid) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0';
+        dfs(row - 1, col, grid);
+        dfs(row + 1, col, grid);
+        dfs(row, col - 1, grid);
+        dfs(row, col + 1, grid);
+    }
+}
